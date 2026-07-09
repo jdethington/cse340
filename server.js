5bbdd4e9707d6c58d099fde869911016fb74e339
jdethington <jdethington@gmail.com>
2026-07-08 17:04:26 -0700

    spaces

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ app.get('/', async (req, res) => {
 
 app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
-
     const title = 'Our Partner Organizations';
-    res.render('organizations', {title, organizations});
+
+    res.render('organizations', { title, organizations });
 })
 
 app.get('/projects', async (req, res) => {
