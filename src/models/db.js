d8323121c03a6f38ca47105160c7ef6b99be0477
jdethington <jdethington@gmail.com>
2026-07-09 19:54:13 -0700

    ssl

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,12 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    // ssl: true
+     ssl: {
+   rejectUnauthorized: false
+  }
+
+    
 });
 
 /**
