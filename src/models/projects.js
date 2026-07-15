80b85ad68147f413f2f3553b160d335df66f6a29
jdethington <jdethington@gmail.com>
2026-07-15 14:20:08 -0700

    Route and Query Parameters finished

--- src/models/projects.js
+++ src/models/projects.js
@@ -4,22 +4,42 @@ import db from './db.js'
 const getAllProjects = async () => {
     const query = `
         SELECT
-            p.project_id,
-            p.title,
+            project_id,
+            title,
             p.description,
-            p.location,
-            TO_CHAR(p.project_date, 'Month DD, YYYY') AS date_long,
-            TO_CHAR(p.project_date, 'MM-DD-YYYY') AS date_short,
+            location,
+            TO_CHAR(project_date, 'Month DD, YYYY') AS date_long,
+            TO_CHAR(project_date, 'MM-DD-YYYY') AS date_short,
             p.organization_id,
-            o.name AS organization_name
-        FROM public.project p
-        INNER JOIN public.organization o ON p.organization_id = o.organization_id
-        ORDER BY p.project_date ;
+            name AS organization_name
+        FROM project p
+        JOIN organization o ON p.organization_id = o.organization_id
+        ORDER BY project_date ;
     `;
  
     const result = await db.query(query);
     return result.rows;
 }
+
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date AS date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
  
-export { getAllProjects }
+export { getAllProjects, getProjectsByOrganizationId }
  
