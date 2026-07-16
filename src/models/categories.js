c46fff36c2a38e536302600b6c5bdf241c83ba12
jdethington <jdethington@gmail.com>
2026-07-16 12:54:11 -0700

    Add Model Functions

--- src/models/categories.js
+++ src/models/categories.js
@@ -6,10 +6,62 @@ const getAllCategories = async () => {
             category_id AS id,
             name,
             description
-        FROM public.category;
+        FROM category;
     `;
     const result = await db.query(query);
     return result.rows;
 };
 
-export { getAllCategories };
+// Retrieve a single category by its ID.
+const getCategory = async () => {
+    const query = `
+        SELECT
+            category_id AS id,
+            name,
+            description
+        FROM category
+        WHERE id = $1;
+    `;
+    const result = await db.query(query);
+    return result.rows;
+};
+
+// Retrieve all categories for a given service project.
+const getAllCategoriesForServiceProject = async () => {
+    const query = `
+        SELECT
+            c.category_id,
+            name,
+            description
+        FROM category c
+        JOIN project_category pc ON c.category_id = pc.category_id
+        WHERE pc.project_id = $1;
+    `;
+    const result = await db.query(query);
+    return result.rows;
+};
+
+// Retrieve all service projects for a given category.
+const getAllServiceProjectsForCategory = async () => {
+    const query = `
+        SELECT 
+            p.project_id,
+            p.title,
+            p.description,
+            p.location,
+            p.project_date,
+            p.organization_id,
+            o.name AS organization_name
+        FROM project p
+        JOIN project_category pc ON p.project_id = pc.project_id
+        JOIN organization o ON p.organization_id = o.organization_id
+        WHERE pc.category_id = $1
+        ORDER BY p.project_date DESC;
+    `;
+    const result = await db.query(query);
+    return result.rows;
+};
+
+
+
+export { getAllCategories, getCategory, getAllCategoriesForServiceProject, getAllServiceProjectsForCategory };
